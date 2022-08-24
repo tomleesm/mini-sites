1d860cda44540eae62679c012da29ba198835096
Tom <tomleesm@gmail.com>
2022-08-24 21:30:28 +0800

    新增 view posts.create

---
+++ 002/resources/views/posts/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <form method="post" action="{{ route('posts.store') }}">
+                @csrf
+                <p>
+                    <input type="text" name="title" placeholder="文章標題" required autofocus>
+                </p>
+                <p>
+                    <textarea name="content" placeholder="文章內容"></textarea>
+                </p>
+
+                <p>
+                    <button type="submit">儲存</button>
+                    <a href="{{ route('home') }}">取消</a>
+                </p>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
