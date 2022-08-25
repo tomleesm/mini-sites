ca74d0c9b8d8aa2ceee2b5182b848664341887e1
Tom <tomleesm@gmail.com>
2022-08-26 00:32:32 +0800

    登入後右上方的下拉選單加上後台文章清單連結

--- 002/resources/views/layouts/app.blade.php
+++ 002/resources/views/layouts/app.blade.php
@@ -55,6 +55,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('posts.index', [ session('pageName') => session('currentPage') ]) }}">文章清單</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
