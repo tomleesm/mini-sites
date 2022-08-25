7d32e2089b9035217dd23ec93d913ff153cb0aa1
Tom <tomleesm@gmail.com>
2022-08-25 23:22:58 +0800

    前臺文章清單加上標題

--- 002/resources/views/users/posts.blade.php
+++ 002/resources/views/users/posts.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <h1>{{ $user->name }} 的文章清單</h1>
             @foreach($posts as $post)
                 <p>
                     <a href="{{ route('users.posts.show', [ 'user' => $user->id, 'post' => $post->id ]) }}">{{ $post->title }}</a>
