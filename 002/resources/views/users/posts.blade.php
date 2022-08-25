d5b80467783141270b9139d6ccdce0f6c72a476b
Tom <tomleesm@gmail.com>
2022-08-25 23:14:36 +0800

    前臺文章清單共用 view layouts.app

--- 002/resources/views/users/posts.blade.php
+++ 002/resources/views/users/posts.blade.php
@@ -1,16 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>{{ $user->name }} 的所有文章</title>
-</head>
-<body>
-    @foreach($posts as $post)
-        <p>
-            <a href="{{ route('users.posts.show', [ 'user' => $user->id, 'post' => $post->id ]) }}">{{ $post->title }}</a>
-        </p>
-    @endforeach
+@extends('layouts.app')
 
-    {{ $posts->links() }}
-</body>
-</html>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @foreach($posts as $post)
+                <p>
+                    <a href="{{ route('users.posts.show', [ 'user' => $user->id, 'post' => $post->id ]) }}">{{ $post->title }}</a>
+                </p>
+            @endforeach
+
+            {{ $posts->links() }}
+        </div>
+    </div>
+</div>
+@endsection
