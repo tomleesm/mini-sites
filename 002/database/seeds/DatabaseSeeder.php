057f09c8273450848071e0f0f8b0929b73f26cef
Tom <tomleesm@gmail.com>
2022-08-25 19:04:32 +0800

    建立 users 和 posts 的 seeder

--- 002/database/seeds/DatabaseSeeder.php
+++ 002/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = \Faker\Factory::create();
+        $hash = bcrypt('12345678');
+
+        $userId = DB::table('users')->insertGetId([
+            'name' => $faker->name(),
+            'email' => $faker->unique()->email(),
+            'password' => $hash,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        for($i = 0; $i < 105; $i++) {
+            DB::table('posts')->insert([
+                'title' => $faker->sentence(),
+                'content' => $faker->text(),
+                'created_at' => now(),
+                'updated_at' => now(),
+                'user_id' => $userId
+            ]);
+        }
     }
 }
