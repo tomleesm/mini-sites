ca00b35c2ee4479ab27790fe95c43c53c85e7cda
Tom <tomleesm@gmail.com>
2022-08-25 23:17:49 +0800

    前端文章共用 view layouts.app

--- 002/resources/views/users/post.blade.php
+++ 002/resources/views/users/post.blade.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>{{ $post->title }}</title>
-</head>
-<body>
-    <h1>{{ $post->title }}</h1>
-    <article>
-        {{ $post->content }}
-    </article>
-    <p>新增文章時間：{{ $post->created_at }}</p>
-    <p>修改文章時間：{{ $post->updated_at }}</p>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1>{{ $post->title }}</h1>
+            <article>
+                {{ $post->content }}
+            </article>
+            <p>新增文章時間：{{ $post->created_at }}</p>
+            <p>修改文章時間：{{ $post->updated_at }}</p>
+        </div>
+    </div>
+</div>
+@endsection
