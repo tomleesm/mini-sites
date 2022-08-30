980472fa40ab784382b0112bc2a27a921da7ca67
Tom <tomleesm@gmail.com>
2022-08-30 09:34:25 +0800

    圖片路徑放到 class property filePath

--- 003/app/Http/Controllers/PhotoController.php
+++ 003/app/Http/Controllers/PhotoController.php
@@ -8,17 +8,23 @@ use Illuminate\Support\Log;
 
 class PhotoController extends Controller
 {
+    # 儲存檔案到類似 storage/app/2022-08-28/x9wyHUII5Jlsulnb/
+    private $filePath;
+
+    public function __construct() {
+        $this->filePath = sprintf('%s/%s/', today()->toDateString(), Str::random());
+    }
+
     public function store(Request $request) {
         $files = $request->file('uploadPhotos');
 
-        # 儲存檔案到類似 storage/app/2022-08-28/x9wyHUII5Jlsulnb/origin/
-        $path = sprintf('%s/%s/origin/', today()->toDateString(), Str::random());
         foreach($files as $file) {
             if($request->hasFile('uploadPhotos') && $file->isValid()) {
-                $file->store($path);
+                $file->store($this->filePath . 'origin');
             } else {
                 Log::error('無法儲存檔案');
             }
         }
+
     }
 }
