fafb6331412824b9fe0d7839d37af0954a1a749d
Tom <tomleesm@gmail.com>
2022-08-31 21:26:37 +0800

    Log 的 use 寫錯了

--- 003/app/Http/Controllers/PhotoController.php
+++ 003/app/Http/Controllers/PhotoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
-use Illuminate\Support\Log;
+use Illuminate\Support\Facades\Log;
 
 class PhotoController extends Controller
 {
