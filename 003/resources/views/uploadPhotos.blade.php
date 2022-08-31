e312e32a9e0016b8885121384e26b7cae00c892e
Tom <tomleesm@gmail.com>
2022-09-01 06:17:18 +0800

    說明上傳圖片限制

--- 003/resources/views/uploadPhotos.blade.php
+++ 003/resources/views/uploadPhotos.blade.php
@@ -7,8 +7,8 @@
 <body>
     <form method="post" action="{{ route('photos.store') }}" enctype="multipart/form-data">
         @csrf
-        <input type="file" name="uploadPhotos[]" multiple multiple accept="image/gif,image/jpeg,image/png">
-        <button type="submit">上傳圖片</button>
+        <p><input type="file" name="uploadPhotos[]" multiple accept="image/gif,image/jpeg,image/png"><button type="submit">上傳圖片</button></p>
+        <p>上傳圖片限制：每張圖片 10 MB，最多 50 張，總共最多 100 MB，類型爲 JPG、GIF 和 PNG</p>
     </form>
 </body>
 </html>
