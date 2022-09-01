b5ce0f1dc411304a8a61fff3b7ac78d0757e158b
Tom <tomleesm@gmail.com>
2022-09-01 22:14:31 +0800

    如果圖片已刪除，則顯示「圖片已刪除」

--- 003/resources/views/photos/show.blade.php
+++ 003/resources/views/photos/show.blade.php
@@ -5,12 +5,16 @@
     <title>003 相簿</title>
 </head>
 <body>
-    <h3>圖片將在 {{ session('runQueueTime') }} 刪除</h3>
-   @foreach($images as $image)
-       <a data-fslightbox="gallery" href="{{ $image['origin'] }}">
-           <img src="{{ $image['thumbnail'] }}">
-       </a>
-   @endforeach
+    @if(empty($images))
+        <h3>圖片已刪除</h3>
+    @else
+        <h3>圖片將在 {{ session('runQueueTime') }} 刪除</h3>
+        @foreach($images as $image)
+            <a data-fslightbox="gallery" href="{{ $image['origin'] }}">
+                <img src="{{ $image['thumbnail'] }}">
+            </a>
+        @endforeach
+    @endif
 </body>
     <script src="{{ mix('/js/app.js') }}"></script>
 </html>
