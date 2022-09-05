82ad75ec101746108f6be1569562fb8926ab443a
Tom <tomleesm@gmail.com>
2022-09-05 19:35:38 +0800

    在 Laravel 的前端設定 Plyr

---
+++ 004/resources/js/plyr.js
@@ -0,0 +1,3 @@
+import Plyr from 'plyr';
+
+const plyr = new Plyr('#player');
---
+++ 004/resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <video id="player" controls>
+        <source src="/test.mp4" type="video/mp4" />
+    </video>
+</body>
+<script src="{{ mix('js/app.js') }}"></script>
+</html>
