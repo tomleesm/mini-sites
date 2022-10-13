c9ecf1a00526e53f7af1ec156756ad084daa1558
Tom <tomleesm@gmail.com>
2022-10-13 16:08:11 +0800

    把 JavaScript 程式碼包到 ChatRoom 物件中

--- 004/resources/views/app.blade.php
+++ 004/resources/views/app.blade.php
@@ -25,9 +25,36 @@
     <script>
       var ws = new WebSocket('ws://192.168.56.10:8080');
 
-      var form = document.getElementById('form');
-      var input = document.getElementById('input');
-      var messages = document.getElementById('messages');
+      const ChatRoom = {
+          // 輸入訊息的表單
+          form: document.getElementById('form'),
+          // 輸入訊息的 input
+          input: document.getElementById('input'),
+          // 聊天室訊息
+          messages: document.getElementById('messages')
+      };
+      ChatRoom.addMessage = function(message) {
+        // 把訊息加到聊天室結尾
+        var item = document.createElement('li');
+        item.textContent = message;
+        this.messages.appendChild(item);
+        // 視窗捲軸最底下
+        window.scrollTo(0, document.body.scrollHeight);
+      }
+      ChatRoom.submitMessage = function() {
+        this.form.addEventListener('submit', function(e) {
+          e.preventDefault();
+          if (this.input.value) {
+            ws.send(this.input.value);
+
+            ChatRoom.addMessage(this.input.value);
+
+            this.input.value = '';
+          }
+        });
+      }
+
+      ChatRoom.submitMessage();
 
       ws.onopen = function() {
         console.log("Connection open");
@@ -35,28 +62,12 @@
       ws.onmessage = function(event) {
         console.log( "Received message from server: " + event.data);
 
-        var item = document.createElement('li');
-        item.textContent = event.data;
-        messages.appendChild(item);
-        window.scrollTo(0, document.body.scrollHeight);
+        ChatRoom.addMessage(event.data);
       };
       ws.onclose = function() {
         console.log("Connection closed.");
       };
 
-      form.addEventListener('submit', function(e) {
-        e.preventDefault();
-        if (input.value) {
-          ws.send(input.value);
-
-          var item = document.createElement('li');
-          item.textContent = input.value;
-          messages.appendChild(item);
-          window.scrollTo(0, document.body.scrollHeight);
-
-          input.value = '';
-        }
-      });
     </script>
 </body>
 </html>
