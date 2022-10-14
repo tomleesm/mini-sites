2657e5062c0ff73f21013b5e87460e2e744a5f18
Tom <tomleesm@gmail.com>
2022-10-14 18:19:17 +0800

    登入或註冊後才會顯示訊息輸入框

--- 004/resources/views/app.blade.php
+++ 004/resources/views/app.blade.php
@@ -2,7 +2,11 @@
 
 @section('content')
     <ul id="messages"></ul>
+    @guest
+        <p id="join-message">{{ __('Login') }} or {{ __('Register') }} to Join</p>
+    @else
     <form id="form" action="">
         <input id="input" autocomplete="off" /><button>Send</button>
     </form>
+    @endguest
 @endsection
